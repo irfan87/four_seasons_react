109ebfb0c7e46f2ca0eeab1ff1807c42c109d595
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-11 17:29:54 +0800

    refactored the code from functional components to class-base component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { Component } from "react";
 import "./App.css";
+import SeasonDisplay from "./components/SeasonDisplay";
 
-function App() {
-	return (
-		<div className="app">
-			<h1>Four Seasons React</h1>
-		</div>
-	);
+class App extends Component {
+	render() {
+		// get the user current location
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				console.log("current position", position);
+			},
+			(err) => console.error(err)
+		);
+
+		return (
+			<div className="app">
+				<h2>Latitude: </h2>
+				<SeasonDisplay />
+			</div>
+		);
+	}
 }
 
 export default App;
