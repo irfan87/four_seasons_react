ab0f4930d2753a34e2e8165fbc4146316509c393
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-11 21:16:00 +0800

    get the current user location with Geolocation API

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import "./App.css";
 import SeasonDisplay from "./components/SeasonDisplay";
 
 class App extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+
+		// lat is latitude
+		this.state = { lat: null };
+
 		// get the user current location
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
-				console.log("current position", position);
+				// to update or call the object, use this.setState({})
+				this.setState({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				});
 			},
 			(err) => console.error(err)
 		);
+	}
 
+	render() {
 		return (
 			<div className="app">
-				<h2>Latitude: </h2>
+				<h2>Latitude: {this.state.lat}</h2>
+				<h2>Longitude: {this.state.lng}</h2>
 				<SeasonDisplay />
 			</div>
 		);
