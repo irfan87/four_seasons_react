a79c8c7606948783b829a4b35141607dc3db743d
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-11 22:51:22 +0800

    determine the season based on the user's location

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,6 +1,18 @@
 import React from "react";
 
+// determine the season
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? "summer" : "winter";
+	} else {
+		return lat < 0 ? "winter" : "summer";
+	}
+};
+
 const SeasonDisplay = ({ latitude, longitude }) => {
+	const season = getSeason(latitude, new Date().getMonth());
+	console.log(season);
+
 	return (
 		<div className="seasonDisplay">
 			<h1>Season Display</h1>
