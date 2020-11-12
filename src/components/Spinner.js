8bcd3e1e615a07a22d6991879446b341db55f80e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-12 11:50:58 +0800

    use defaultProps for enable message as default at Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Spinner = () => {
+// if we don't provide message prop from parent - in this case, App.js,
+// we set message as the defaultProps
+const Spinner = ({ message }) => {
 	return (
 		<div className="ui active dimmer">
-			<div className="ui big text loader">Loading</div>
+			<div className="ui big text loader">{message}</div>
 		</div>
 	);
+};
+
+Spinner.defaultProps = {
+	message: "Loading",
 };
 
 export default Spinner;
